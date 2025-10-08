bd95f77b4aa0a3d38d21e0fa84d213dd4ec9ba6c
Tanvinhza223 <tanvinhza2@gmail.com>
2025-10-08 15:55:44 +0700

    Lab3_70%

---
+++ src/main/java/org/AccountService.java
@@ -0,0 +1,18 @@
+package org;
+
+import java.util.regex.Pattern;
+
+public class AccountService {
+    private static final Pattern EMAIL_PATTERN =
+            Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+    public boolean registerAccount(String username, String password, String email) {
+        if (username == null || username.isBlank()) return false;
+        if (password == null || password.length() <= 6) return false;
+        if (!isValidEmail(email)) return false;
+        return true;
+    }
+    public boolean isValidEmail(String email) {
+        if (email == null) return false;
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+}
---
+++ src/test/java/AccountServiceTest.java
@@ -0,0 +1,26 @@
+import org.AccountService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AccountServiceTest {
+    private final AccountService service = new AccountService();
+
+    @DisplayName(" registration ")
+    @ParameterizedTest(name = "[{index}] username={0}, password={1}, email={2} => expected={3}")
+    @CsvFileSource(resources = "/test-data.csv", numLinesToSkip = 1)
+    void registrationFromCsv(String username, String password, String email, boolean expected) {
+        boolean actual = service.registerAccount(username, password, email);
+        assertEquals(expected, actual, "pass must have 8 character at least");
+    }
+
+    @Test
+    @DisplayName(" Email validation quick checks")
+    void emailValidation() {
+        Assertions.assertTrue(service.isValidEmail("ok@test.com"));
+        Assertions.assertFalse(service.isValidEmail("bad.com"));
+        Assertions.assertFalse(service.isValidEmail(null));
+    }
+}
